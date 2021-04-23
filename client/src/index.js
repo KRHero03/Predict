dedccfa38a1c9a0a2d12236c4bd9a0d75e704b70
KRHero03 <krunalhero@gmail.com>
2021-04-23 11:57:19 +0530

    Deleted Extra files

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
@@ -10,22 +8,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 2c46d34d7006726a2f9390c85d2c4a4204a6ea0f
